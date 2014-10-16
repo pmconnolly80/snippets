bc20f206050b5cb2f767932a10b235aabb7394c6
ngworkshop <hello@ng-workshop.com>
2014-10-16 16:48:28 +0200

    changes

--- init/app.js
+++ init/app.js
@@ -2,51 +2,77 @@
 
   angular.module('app', [])
 
-    .factory('AppService', function ($http, $window, $document, $q) {
+    // Google OAuth Client ID ("ng-workshop" project)
+    .constant('GAPI_CLIENT_ID', '178943113374-d5f5mska0vvb956jb1e8sstf8dgqbqrh.apps.googleusercontent.com')
 
-      var gapi,
-          initComplete = $q.defer();  // Promise that will be resolved when our app has finished initializing
+    .factory('AppService', function ($window, $document, $q, $timeout, GAPI_CLIENT_ID) {
 
-      // Trigger initialization
-      init();
+      var initComplete = $q.defer();  // Global promise that wraps all initialization promises
+
+      // This is how you would trigger initialization in a real app.
+      // For this tutorial, the user must manually call `startInit`.
+      // init();
 
       // Public interface
       return {
-        isReady: isReady
+        isReady: isReady,
+        startInit: startInit
       };
 
       /**
-       * Initialize the app:
-       *   - Load Google SDK
-       *   - Assign `gapi`
-       *   - Check user auth status
-       *sdfds
-       *
-       * And then resolve the promise indicating that our app has finished initializing.
-       */
-      function init() {
-        var isGoogleLoaded;
-
-        // Load the Google JavaScript client library.
-        isGoogleLoaded = loadJsLibrary('google-jssdk', 'https://apis.google.com/js/client.js?onload=handleClientLoad', 'handleClientLoad');
-        isGoogleLoaded.then(function() {
-          console.log('Google loaded');
-        });
-
-
-        // gapi = $window.gapi;
-      }
-
-      /**
-       * Return a promise that will be resolved when AppService is ready,
-       * i.e. the Google SDK has been loaded and the user auth status has been checked.
+       * Return a promise that will be resolved when everything is initialized.
        */
       function isReady() {
         return initComplete.promise;
       }
 
       /**
-       * Load an external JS library.
+       * You wouldn't normally expose the init() method in a real app,
+       * but we want to let the user trigger initialization for the tutorial.
+       */
+      function startInit() {
+        init();
+      }
+
+      /**
+       * Initialize the app:
+       *   - Load Google SDK
+       *   - Load Facebook SDK
+       *   - Check if user is authenticated with Google
+       *
+       * When all three tasks are complete, the global initialization promise will be resolved.
+       */
+      function init() {
+        var GoogleSdkLoaded, FacebookSdkLoaded,
+            GoogleUserStatusDeferred = $q.defer();
+
+        // Load the Google JS SDK.
+        GoogleSdkLoaded = loadJsLibrary('google-jssdk', 'https://apis.google.com/js/client.js?onload=handleClientLoad', 'handleClientLoad');
+
+        // If Google SDK was loaded, get the user auth status
+        // from Google using "immediate mode" (no UI is shown).
+        GoogleSdkLoaded.then(function() {
+          var authParams = {client_id: GAPI_CLIENT_ID, scope: 'profile', immediate: true};
+          $window.gapi.auth.authorize(authParams, function(authResult) {
+            GoogleUserStatusDeferred.resolve(authResult);
+          });
+        });
+
+        // Load the Facebook JS SDK.
+        FacebookSdkLoaded = loadJsLibrary('facebook-jssdk', '//connect.facebook.net/en_US/sdk.js', 'fbAsyncInit');
+
+        // Resolve the global initialization promise if all three sub-promises are resolved,
+        // or reject it if one sub-promise is rejected.
+        $q.all([GoogleSdkLoaded, FacebookSdkLoaded, GoogleUserStatusDeferred.promise]).then(function() {
+          // We pass the resolve values of all promises (in `arguments`) to the master promise.
+          initComplete.resolve(arguments);
+        }, function() {
+          initComplete.reject();
+        });
+      }
+
+      /**
+       * Helper function. Load an external JS library.
        *
        * Create a global function that will act as the callback for the library being loaded.
        * When called, this function will resolve a promise and delete itself.
@@ -54,15 +80,19 @@
       function loadJsLibrary(id, src, callback) {
         var deferred = $q.defer();
         $window[callback] = function() {
-          deferred.resolve();
+          deferred.resolve('Library loaded: ' + src);
           delete $window[callback];
         };
         addScript(id, src);
+        // If the JS library hasn't called the callback after 5 seconds, the promise will be rejected.
+        // $timeout(function() {
+        //   deferred.reject();
+        // }, 5000);
         return deferred.promise;
       }
 
       /**
-       * Load an external JS file by adding a <script> tag to the current page.
+       * Helper function. Load an external JS file by adding a <script> tag to the current page.
        * Once loaded, the JS file should invoke a global callback function.
        *
        * Based on https://developers.facebook.com/docs/javascript/quickstart
@@ -78,7 +108,22 @@
       }
     })
 
-    .controller('AppCtrl', function(AppService) {
+    .controller('AppCtrl', function($scope, AppService) {
+      // Log useful info to the screen.
+      $scope.log = 'Doing nothing. Please click startInit().';
+
+      // This method is just for the tutorial. Don't do this in a real app.
+      $scope.startInit = function() {
+        $scope.log = 'Initializing...';
+        AppService.startInit();
+      };
+
+      AppService.isReady().then(function(outcomes) {
+        $scope.log = outcomes[0];
+        // console.log('Google & Facebook SDKs loaded. Google authResult is:', authResult);
+      }, function() {
+        $scope.log = 'ERROR: The app could not be initialized.';
+      });
 
     });
 
